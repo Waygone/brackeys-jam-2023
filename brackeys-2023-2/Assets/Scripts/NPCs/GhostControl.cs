907ab250ff6fe629f3750a91f0e9631670602f4b
kindred008 <talia.preston123@gmail.com>
2023-08-23 19:42:46 +0100

    Ghost control working
    - Ghost Control can now send specific dialogue and hints to the Dialogue Manager, based on what quest/objective the player is on

--- brackeys-2023-2/Assets/Scripts/NPCs/GhostControl.cs
+++ brackeys-2023-2/Assets/Scripts/NPCs/GhostControl.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GhostControl : MonoBehaviour
 {
-    private DialogueDB dialogueDB;
-    private NpcMovement ghostMover;
+    private DialogueDB dialogueDb;
+    [SerializeField] private DialogueManager dialogueManager;
+
+    [Tooltip("Which dialogue will display when trying to talk to the Ghost during quests")]
+    [SerializeField] private DialogueChoice[] dialogueChoice;
 
     private void Awake()
     {
-        dialogueDB = GetComponent<DialogueDB>();
+        dialogueDb = GetComponent<DialogueDB>();
+    }
+
+    public void TalkTo()
+    {
+        var dialogueCon = dialogueChoice.LastOrDefault(x => x.ValidDialogue());
+        var dialogueId = dialogueCon.dialogueId;
+
+        if (dialogueDb.TryGetDialogue(dialogueId, out Dialogue dialogue))
+        {
+            SendDialogue(dialogue);
+        }
     }
 
     public void GetHint()
     {
+        var hintText = QuestManager.instance.currentQuest.currentObjective.HintText;
+        if (!string.IsNullOrEmpty(hintText))
+        {
+            var hintDialogue = CreateDialogue("Ghost", hintText, 0.1f);
 
+            SendDialogue(hintDialogue);
+        }
+    }
+
+    private void SendDialogue(Dialogue dialogue)
+    {
+        if (dialogueManager.TrySetDialogue(dialogue))
+        {
+            dialogueManager.PlayDialogue();
+        }
     }
 
     private Dialogue CreateDialogue(string title, string message, float secondsPerCharacter)
@@ -27,4 +56,20 @@ public class GhostControl : MonoBehaviour
 
         return new Dialogue { Id = "na", DialoguePassages = new DialoguePassage[] { dialoguePassage } };
     }
+}
+
+[System.Serializable]
+public struct DialogueChoice
+{
+    public string dialogueId;
+    public string questId;
+    public string objectiveId;
+
+    public bool ValidDialogue()
+    {
+        return CorrectQuest() && CorrectObjective();
+    }
+
+    private bool CorrectQuest() => !string.IsNullOrEmpty(questId) ? QuestManager.instance.currentQuest.QuestId == questId : true;
+    private bool CorrectObjective() => !string.IsNullOrEmpty(objectiveId) ? QuestManager.instance.currentQuest.currentObjective.ObjectiveID == objectiveId : true;
 }
