9b32696d693219544436ea6f287bfd6ce22d5f24
Penca53 <pencapencator@gmail.com>
2023-08-23 23:42:33 +0200

    Add LevelManager. - Add LevelManager to navigate between scenes more easily

---
+++ brackeys-2023-2/Assets/Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    private int _currentLevel;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public bool TryAdvanceToNextLevel()
+    {
+        if (_currentLevel + 1 >= SceneManager.sceneCount)
+        {
+            return false;
+        }
+
+        SceneManager.LoadScene(++_currentLevel);
+        return true;
+    }
+    public bool TryJumpToLevel(int levelIndex)
+    {
+        if (levelIndex < 0 || levelIndex >= SceneManager.sceneCount)
+        {
+            return false;
+        }
+
+        SceneManager.LoadScene(levelIndex);
+        return true;
+    }
+}
