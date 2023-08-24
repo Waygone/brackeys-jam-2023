e6a6af2486150c6d337360f3765b266d805d8538
Penca53 <pencapencator@gmail.com>
2023-08-24 22:00:43 +0200

    Add MainMenu level. - Add MainCanvas to show initial screen with buttons and volume settings. - Add CreditsCanvas to show credits.

---
+++ brackeys-2023-2/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas _MainCanvas;
+    [SerializeField]
+    private Canvas _CreditsCanvas;
+
+    [SerializeField]
+    private Button _MainPlayButton;
+    [SerializeField]
+    private Button _MainCreditsButton;
+    [SerializeField]
+    private Button _MainQuitButton;
+    [SerializeField]
+    private Slider _MainMainVolumeSlider;
+    [SerializeField]
+    private Slider _MainMusicVolumeSlider;
+    [SerializeField]
+    private Button _CreditsBackButton;
+
+    private void Start()
+    {
+        _MainPlayButton.onClick.AddListener(MainCanvasClickPlayHandler);
+        _MainCreditsButton.onClick.AddListener(MainCanvasClickCreditsHandler);
+        _MainQuitButton.onClick.AddListener(MainCanvasClickQuitHandler);
+        _MainMainVolumeSlider.onValueChanged.AddListener(MainMainVolumeSliderChangeHandler);
+        _MainMusicVolumeSlider.onValueChanged.AddListener(MainMusicVolumeSliderChangeHandler);
+
+        _CreditsBackButton.onClick.AddListener(CreditCanvasClickBackHandler);
+
+        _MainMainVolumeSlider.value = GlobalData.MainVolume;
+        _MainMusicVolumeSlider.value = GlobalData.MusicVolume;
+    }
+
+    private void MainCanvasClickPlayHandler()
+    {
+        LevelManager.Instance.TryAdvanceToNextLevel();
+    }
+    private void MainCanvasClickCreditsHandler()
+    {
+        _MainCanvas.enabled = false;
+        _CreditsCanvas.enabled = true;
+    }
+    private void MainCanvasClickQuitHandler()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    private void MainMainVolumeSliderChangeHandler(float value)
+    {
+        GlobalData.MainVolume = (int)value;
+        Debug.Log("Main volume " + GlobalData.MainVolume);
+    }
+    private void MainMusicVolumeSliderChangeHandler(float value)
+    {
+        GlobalData.MusicVolume = (int)value;
+        Debug.Log("Music volume " + GlobalData.MusicVolume);
+    }
+
+    private void CreditCanvasClickBackHandler()
+    {
+        _CreditsCanvas.enabled = false;
+        _MainCanvas.enabled = true;
+    }
+}
