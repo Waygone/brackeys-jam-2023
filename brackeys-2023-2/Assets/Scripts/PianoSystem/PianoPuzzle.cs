b1d17c4e8cfb7b1077e89fbd403d131427536f71
Penca53 <pencapencator@gmail.com>
2023-08-27 01:48:02 +0200

    Implement most of level3 logic. Quest still missing.

--- brackeys-2023-2/Assets/Scripts/PianoSystem/PianoPuzzle.cs
+++ brackeys-2023-2/Assets/Scripts/PianoSystem/PianoPuzzle.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 
 public class PianoPuzzle : MonoBehaviour
 {
-    public delegate void PianoPuzzleSolvedHandler();
+    public delegate void PianoPuzzleSolvedHandler(int phase);
     public event PianoPuzzleSolvedHandler OnPianoPuzzleSolved;
 
-    public delegate void PianoPuzzleWrongHandler();
+    public delegate void PianoPuzzleWrongHandler(int phase);
     public event PianoPuzzleWrongHandler OnPianoPuzzleWrong;
 
     [SerializeField]
     private PianoManager _PianoManager;
     [SerializeField]
+    private DialogueDB _DialogueDB;
+    [SerializeField]
+    private DialogueManager _DialogueManager;
+    [SerializeField]
     private string[] _Sequence;
 
 
+    private bool _isFirstPhase = true;
     private int _currentSequenceIndex;
 
     private void Start()
@@ -31,21 +36,33 @@ public class PianoPuzzle : MonoBehaviour
 
         Debug.Log("Key press " + pianoKey.Key);
 
-        if (pianoKey.Key == _Sequence[_currentSequenceIndex])
+        int index = _currentSequenceIndex;
+        if (_isFirstPhase)
+        {
+            index = _Sequence.Length - _currentSequenceIndex - 1;
+        }
+
+        if (pianoKey.Key == _Sequence[index])
         {
             Debug.Log("Key correct");
             ++_currentSequenceIndex;
             if (_currentSequenceIndex >= _Sequence.Length)
             {
                 _currentSequenceIndex = 0;
-                OnPianoPuzzleSolved?.Invoke();
+                OnPianoPuzzleSolved?.Invoke(_isFirstPhase ? 0 : 1);
+                _isFirstPhase = false;
             }
         }
         else
         {
             Debug.Log("Key wrong");
             _currentSequenceIndex = 0;
-            OnPianoPuzzleWrong?.Invoke();
+            OnPianoPuzzleWrong?.Invoke(_isFirstPhase ? 0 : 1);
         }
+    }
+
+    public void Reset()
+    {
+        _currentSequenceIndex = 0;
     }
 }
