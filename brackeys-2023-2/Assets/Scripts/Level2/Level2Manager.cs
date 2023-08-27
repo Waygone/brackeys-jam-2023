313e9e0d5709fc7cb68116c4bf5d231320930d42
kindred008 <talia.preston123@gmail.com>
2023-08-27 04:45:32 +0100

    Working level 2

--- brackeys-2023-2/Assets/Scripts/Level2/Level2Manager.cs
+++ brackeys-2023-2/Assets/Scripts/Level2/Level2Manager.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using static DialogueManager;
 
 public class Level2Manager : MonoBehaviour
 {
@@ -7,10 +9,48 @@ public class Level2Manager : MonoBehaviour
 
     private readonly bool[] _hasFoundKeys = new bool[3];
 
+    [SerializeField] private Quest quest;
+    [SerializeField] private PlayerController playerController;
+
+    private DialogueDB dialogueDb;
+    private DialogueManager dialogueManager;
+
+    private void Awake()
+    {
+        dialogueDb = GetComponent<DialogueDB>();
+        dialogueManager = GetComponent<DialogueManager>();
+
+        OnLevel2FoundAllKeys += FoundAllKeys;
+    }
+
+    private void Start()
+    {
+        dialogueManager.OnDialogueEnd += DialogueEndHandler;
+        dialogueManager.OnDialogueBegin += DialogueBeginHandler;
+
+        StartCoroutine(StartQuest());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            dialogueManager.SkipPassage();
+        }
+    }
+
     public void SetFoundKey(int index)
     {
         _hasFoundKeys[index] = true;
         UpdateState();
+    }
+
+    private void PlayDialogue(string dialogueId)
+    {
+        if (dialogueManager.TrySetDialogue(dialogueDb.GetDialogue(dialogueId)))
+        {
+            dialogueManager.PlayDialogue();
+        }
     }
 
     private void UpdateState()
@@ -25,5 +65,34 @@ public class Level2Manager : MonoBehaviour
 
         OnLevel2FoundAllKeys?.Invoke();
         Debug.LogWarning("Level2 completed!");
+    }
+
+    private void FoundAllKeys()
+    {
+        QuestManager.instance.TriggerQuestObj("Keys1");
+        PlayDialogue("keys_2");
+    }
+
+    private void DialogueBeginHandler(Dialogue dialogue)
+    {
+        playerController.TogglePlayerControls(false);
+    }
+
+    private void DialogueEndHandler(Dialogue dialogue)
+    {
+        playerController.TogglePlayerControls(true);
+
+        switch (dialogue.Id)
+        {
+            case "keys_1":
+                QuestManager.instance.BeginQuest(quest);
+                break;
+        }
+    }
+
+    private IEnumerator StartQuest()
+    {
+        yield return null;
+        PlayDialogue("keys_1");
     }
 }
